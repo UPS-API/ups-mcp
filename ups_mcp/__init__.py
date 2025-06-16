17edb926d821dd2c31c8021316e74949118f6080
gcz3zgv <alexanderpalacios@ups.com>
2025-06-16 11:50:52 -0400

    Changing project structure for testing

---
+++ ups_mcp/__init__.py
@@ -0,0 +1,7 @@
+"""
+UPS MCP Server
+
+A UPS Model Context Protocol server to enable AI systems to interact with UPS services.
+"""
+
+from .server import main
---
+++ ups_mcp/server.py
@@ -0,0 +1,37 @@
+from typing import Any
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+from . import tools
+
+# Initialize FastMCP server
+mcp = FastMCP("ups-mcp")
+
+@mcp.tool()
+async def track_package(inquiryNumber: str, locale:str="en_US", returnSignature:bool=False, returnMilestones:bool=False, returnPOD:bool=False) -> str:
+    """
+    The Track API retrieves current status of shipments such as Small Package 1Z, Infonotice, Mail Innovations, FGV, or UPS Freight shipments
+    using the inquiry number. The tracking response data typically includes package movements/activities, destination UPS access point
+    information, expected delivery dates/times, etc. The response returns an array of shipment objects containing detailed tracking information 
+    and status for the package(s) associated with the inquiryNumber, including current status, activity history, delivery details, package details, and more.
+    
+    Args:
+        inquiryNumber (str): the unique package identifier. Each inquiry number must be between 7 and 34 characters in length. Required.
+        locale (str): Language and country code of the user, separated by an underscore. Default value is 'en_US'. Not required.
+        returnSignature (bool): a boolean to indicate whether a signature is required, default is false. Not required.
+        returnMilestones (bool): a boolean to indicate whether detailed information on a package's movements is required, default is false. Not required
+        returnPOD (bool): a boolean to indicate whether a proof of delivery is required, default is false. Not required
+
+    Returns:
+        str: The response from the tracking capability, this is a string of json tracking data.
+    """
+    tracking_data = tools.track_package(inquiryNum=inquiryNumber, locale=locale, returnSignature=returnSignature, returnMilestones=returnMilestones, returnPOD=returnPOD)
+
+    return tracking_data
+
+def main():
+    load_dotenv()
+    print("Starting UPS MCP Server...")
+    mcp.run(transport='stdio')
+
+if __name__ == "__main__":
+    main()
