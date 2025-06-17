956a111ba15aab57c48d3b54e11a2cf3ba16ce34
gcz3zgv <alexanderpalacios@ups.com>
2025-06-17 08:57:30 -0400

    Updated pyproject and file structure to provide an executable for uvx

--- src/ups_mcp/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-UPS MCP Server
-
-A UPS Model Context Protocol server to enable AI systems to interact with UPS services.
-"""
-
-from .server import main
--- src/ups_mcp/constants.py
+++
@@ -1,2 +0,0 @@
-CIE_URL = "https://wwwcie.ups.com"
-PRODUCTION_URL = "https://onlinetools.ups.com"
